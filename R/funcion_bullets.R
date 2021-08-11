18987f06ed8e64be5e1658908209803fdf3f4b06
Temisesba <77352740+Temisesba@users.noreply.github.com>
2021-08-11 11:51:42 -0500

    ok

--- R/funcion_bullets.R
+++ R/funcion_bullets.R
@@ -404,7 +404,7 @@ graficas_tablas<-function(situacion,situacion_mapa){
   a<-(a)
   b<-(b)
 
-  return(grafico1, grafico2, grafico3, grafico4, grafico5, bullets, global_t1,
-         global_t2,a, b)
+  return(list(grafico1, grafico2, grafico3, grafico4, grafico5, bullets, global_t1,
+         global_t2,a, b))
 
 }
