6b2882e900a943da0e773e28c8917fe259963b6f
AllanZamb <alanzambie@gmail.com>
2021-09-14 15:16:47 -0500

    ok

--- R/funcion_bullets.R
+++ R/funcion_bullets.R
@@ -521,7 +521,7 @@ generar_pptx_V2 <- function(bullets, grafico1, global_t1, grafico2, global_t2, g
   Titulo <- ph_location_type(type = "title")
   subTitulo <- ph_location_type(type = "subTitle")
 
-  download.file("https://github.com/Temisesba/P-blico/raw/main/Plantilla.pptx", "productos/Plantilla.pptx")
+  #download.file("https://github.com/Temisesba/P-blico/raw/main/Plantilla.pptx", "productos/Plantilla.pptx")
 
   my_pres <- read_pptx("productos/Plantilla.pptx")
 
